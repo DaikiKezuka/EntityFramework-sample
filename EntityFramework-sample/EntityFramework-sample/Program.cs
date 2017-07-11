f0151c4a1bdc07aa333da79175a79cc75cca3a98
DaikiKezuka <papopapo0250@gmail.com>
2017-07-12 00:50:47 +0900

    step2 Initializer Annotation Fluent API

--- EntityFramework-sample/EntityFramework-sample/Program.cs
+++ EntityFramework-sample/EntityFramework-sample/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace EntityFramework_sample
 {
@@ -11,6 +12,10 @@ namespace EntityFramework_sample
     {
         static void Main(string[] args)
         {
+            #region Initializer
+            Database.SetInitializer(
+                new DropCreateDatabaseIfModelChanges<ShoppingContext>());
+            #endregion
             var product = new Product()
             {
                 Name = "TestItem",
@@ -21,23 +26,41 @@ namespace EntityFramework_sample
                 context.Products.Add(product);
                 context.SaveChanges();
 
-                foreach(var p in context.Products)
-                {
-                    Console.WriteLine(p.ProductId + " " + p.Name + " " + p.Price);
-                }
+                #region Console display
+                //foreach(var p in context.Products)
+                //{
+                //    Console.WriteLine(p.ProductId + " " + p.Name + " " + p.Price);
+                //}
+                #endregion
             }
         }
 
         class Product
         {
             public int ProductId { get; set; }
+            
+            /*Annotation*/
+            //[Required]
+            //[MaxLength(200)]
             public string Name { get; set; }
             public int Price { get; set; }
+            public int Stock { get; set; }
         }
 
         class ShoppingContext : DbContext
         {
             public DbSet<Product> Products { get; set; }
+
+            /*Fluent API*/
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+
+                modelBuilder.Entity<Product>()
+                    .Property(d => d.Name)
+                    .IsRequired()
+                    .HasMaxLength(200);
+            }
         }
     }
 }
